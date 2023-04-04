6152e8c1c65654fc8c08d50802964834b456e443
akshaydefault <96437224+akshaydefault@users.noreply.github.com>
2023-04-05 00:28:31 +0530

    Run build command

---
+++ .next/server/chunks/231.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.id = 231;
+exports.ids = [231];
+exports.modules = {
+
+/***/ 231:
+/***/ ((module) => {
+
+module.exports = JSON.parse('[{"id":1,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/horses-2904536_1280+(1).jpg","name":"Horses","location":"Delhi, India","artist_name":"PICASSO","lat":28.6679577,"long":77.2135249,"slug":"horses","isLiked":false},{"id":2,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/circle-with-arrows.jpeg","name":"Circle with Arrow","location":"Noida, India","artist_name":"GIOTTO","lat":28.5221769,"long":77.3194247,"slug":"circle-with-arrow","isLiked":false},{"id":3,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/flower.jpg","name":"Flower","location":"Moradabad, India","artist_name":"LEONARDO","lat":28.8431472,"long":78.7263528,"slug":"flower","isLiked":false},{"id":4,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/monalisa.jpg","name":"Monalisa","location":"Lovure museum, Paris","artist_name":"CÉZANNE","lat":48.8669741,"long":2.3271663,"slug":"monalisa","isLiked":false},{"id":5,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/girly-eye.jpg","name":"Girl Eye","location":"India Gate, India","artist_name":"REMBRANDT","lat":28.612912,"long":77.2269348,"slug":"girl-eye","isLiked":false},{"id":6,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/lion-king-with-full-armor-weapon-generative-ai.jpg","name":"Lion Creator","location":"Gurgaon, India","artist_name":"VELÁZQUEZ","lat":28.4229327,"long":76.9073483,"slug":"lion","isLiked":false},{"id":7,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/shoe.jpg","name":"Shoe","location":"Gateway of India, Mumbai","artist_name":"KANDINSKY","lat":18.9219841,"long":72.8320794,"slug":"shoe","isLiked":false},{"id":8,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/thankyou.jpg","name":"Thankyou","location":"baner, Pune","artist_name":"MONET","lat":18.5599713,"long":73.7735012,"slug":"thankyou","isLiked":false},{"id":9,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/trimurti.jpg","name":"TriMurti","location":"Jim Corbet, India","artist_name":"CARAVAGGIO","lat":29.5300138,"long":78.7720854,"slug":"tri-murti","isLiked":false},{"id":10,"image":"https://akshay-art-gallery.s3.ap-south-1.amazonaws.com/try-speaking.jpg","name":"Try Speaking","location":"Mahabaleshwar, Maharashtra, India","artist_name":"VAN EYCK","lat":17.925821,"long":73.6281821,"slug":"try-speaking","isLiked":false}]');
+
+/***/ })
+
+};
+;
