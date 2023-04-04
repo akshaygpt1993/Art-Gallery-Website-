ed5af4042155e08371a8a9059c65dd100e5177c2
akshaydefault <96437224+akshaydefault@users.noreply.github.com>
2023-04-05 00:48:33 +0530

    Updated config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone',
-    distDir: 'build',
     images: {
         domains: ["akshay-art-gallery.s3.ap-south-1.amazonaws.com"]
     }
